6d962f05ffe3a382711d5c5da2f39efd0b6cfdd0
Tanapol Prucksakorn <zynaxsoft@gmail.com>
2021-02-19 23:42:45 +0900

    Remove unwrap (#573)

--- rust/ballista/src/serde/scheduler/from_proto.rs
+++ rust/ballista/src/serde/scheduler/from_proto.rs
@@ -29,12 +29,19 @@ impl TryInto<Action> for protobuf::Action {
     fn try_into(self) -> Result<Action, Self::Error> {
         match self.action_type {
             Some(ActionType::ExecutePartition(partition)) => {
-                // TODO remove unwraps
                 Ok(Action::ExecutePartition(ExecutePartition::new(
-                    Uuid::parse_str(&partition.job_uuid).unwrap(),
+                    parse_job_uuid(&partition.job_uuid)?,
                     partition.stage_id as usize,
                     partition.partition_id as usize,
-                    partition.plan.as_ref().unwrap().try_into()?,
+                    partition
+                        .plan
+                        .as_ref()
+                        .ok_or_else(|| {
+                            BallistaError::General(
+                                "PhysicalPlanNode in ExecutePartition is missing".to_owned(),
+                            )
+                        })?
+                        .try_into()?,
                     HashMap::new(),
                 )))
             }
@@ -53,7 +60,7 @@ impl TryInto<PartitionId> for protobuf::PartitionId {
 
     fn try_into(self) -> Result<PartitionId, Self::Error> {
         Ok(PartitionId::new(
-            Uuid::parse_str(&self.job_uuid).unwrap(),
+            parse_job_uuid(&self.job_uuid)?,
             self.stage_id as usize,
             self.partition_id as usize,
         ))
@@ -65,8 +72,33 @@ impl TryInto<PartitionLocation> for protobuf::PartitionLocation {
 
     fn try_into(self) -> Result<PartitionLocation, Self::Error> {
         Ok(PartitionLocation {
-            partition_id: self.partition_id.unwrap().try_into()?,
-            executor_meta: self.executor_meta.unwrap().into(),
+            partition_id: self
+                .partition_id
+                .ok_or_else(|| {
+                    BallistaError::General(
+                        "partition_id in PartitionLocation is missing.".to_owned(),
+                    )
+                })?
+                .try_into()?,
+            executor_meta: self
+                .executor_meta
+                .ok_or_else(|| {
+                    BallistaError::General(
+                        "executor_meta in PartitionLocation is missing".to_owned(),
+                    )
+                })?
+                .into(),
         })
+    }
+}
+
+fn parse_job_uuid(job_uuid: &str) -> Result<Uuid, BallistaError> {
+    if let Ok(parsed_uuid) = Uuid::parse_str(&job_uuid) {
+        Ok(parsed_uuid)
+    } else {
+        Err(BallistaError::General(format!(
+            "Invalid job_uuid {}",
+            job_uuid
+        )))
     }
 }
